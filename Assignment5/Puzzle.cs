b4faf4c0d095fef6f9701cb45b792d04e0976226
Ben Simpson <Bsimpson@imo-online.com>
2018-11-08 20:44:48 -0600

    add time property to puzzle class

--- Assignment5/Puzzle.cs
+++ Assignment5/Puzzle.cs
@@ -13,5 +13,6 @@ namespace Assignment5
         public string Start { get; set; }
         public string Progress { get; set; }
         public string Solution { get; set; }
+        public int Time { get; set; }
     }
 }
