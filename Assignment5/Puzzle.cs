34275dbfd1d42ce3a67f1f67c035182d784478c4
Ben Simpson <Bsimpson@imo-online.com>
2018-11-08 20:49:10 -0600

    Added ToString() method for the puzzle class.

--- Assignment5/Puzzle.cs
+++ Assignment5/Puzzle.cs
@@ -14,5 +14,10 @@ namespace Assignment5
         public string Progress { get; set; }
         public string Solution { get; set; }
         public int Time { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", Name, Difficulty, Start, Progress, Solution, Time);
+        }
     }
 }
